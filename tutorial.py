e1af7fc7d20147885f9ee1d8ee2e0969202954fc
ehsan <eaca89@gmail.com>
2024-08-10 17:44:36 -0400

    html files are added

--- tutorial.py
+++ tutorial.py
@@ -1,18 +1,20 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello! this is the main page <h1>HELLO</h1>"
-
+# the pages of our website
 @app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
+def home(name):
+    return render_template("index.html", content=["jack", "sarah", "tom"])
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
+# @app.route("/<name>")
+# def user(name):
+#     return f"Hello {name}!"
+
+# @app.route("/admin")
+# def admin():
+#     # return redirect(url_for("home"))
+#     return redirect(url_for("user", name="Admin!"))
 
 if __name__ == "__main__":
     app.run()
